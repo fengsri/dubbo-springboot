513543e04bf1a68b371e9e8f838dc69ab59eeee1
fengwen <1477452395@qq.com>
2020-01-23 16:20:09 +0800

    提交

--- provider/src/main/java/com/provider/demo/comment/DistributedLock.java
+++ provider/src/main/java/com/provider/demo/comment/DistributedLock.java
@@ -8,7 +8,6 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 /**
